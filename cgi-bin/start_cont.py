c5c7c4e9afee2aca6b19d05e2470683f278b8103
root <root@station223.example.com>
2019-04-23 21:07:59 +0530

    python cgi file to start stopped docker container

---
+++ cgi-bin/start_cont.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+import os
+import cgi
+import commands
+
+print "Content-Type: text/html"
+print "" #left blank to bypass the remaing headers.
+
+#getting html data
+mypage_data=cgi.FieldStorage() #FieldStorage is optin in cgi
+print "Task will be done"
+
+name=mypage_data.getvalue('cont_name')
+image=mypage_data.getvalue('cont_image')
+print "<br/>"
+print name
+print "<br/>"
+print image
+print "<br/>"
+print "</pre>"
+
+x=commands.getoutput("sudo echo  name: "+str(name)+"   > vars.yml ")
+y=commands.getoutput("sudo echo  image: "+str(image)+"   >> vars.yml ")
+
+print x
+print "</br>"
+print y
+print "</br>"
+print "<pre>"
+print os.system("sudo ansible-playbook start_cont.yml")
+print "</pre>"
+print "</br>"
